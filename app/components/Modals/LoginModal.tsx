0bd1b50a4d0ba6e424bc13af2baeafe61f28eb34
Chris Jagernath <jagernat@sheridancollege.ca>
2023-04-23 21:20:09 -0400

    implemented rent modal and categories

--- app/components/Modals/LoginModal.tsx
+++ app/components/Modals/LoginModal.tsx
@@ -53,6 +53,11 @@ const LoginModal = () => {
     });
   };
 
+  const toggle = useCallback(() => {
+    loginModal.onClose();
+    registerModal.onOpen();
+  }, [loginModal, registerModal]);
+
   const bodyContent = (
     <div className="flex flex-col gap-4">
       <Heading title="Welcome back" subtitle="Login to your account!" />
@@ -100,16 +105,16 @@ const LoginModal = () => {
       "
       >
         <div className="justify-center flex flex-row items-center gap-2">
-          <div>Already have an account?</div>
+          <div>First time using Airbnb?</div>
           <div
-            onClick={registerModal.onClose}
+            onClick={toggle}
             className="
           text-neutral-800
           cursor-pointer
           hover:underline
           "
           >
-            Log in
+            Create an account
           </div>
         </div>
       </div>
